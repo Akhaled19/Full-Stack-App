a1fc680d0a4cb0b9e0c63ad1e873ff0a5e6266b3
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-25 16:28:53 -0400

    add call Rest Api from react app test

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
+import config from './config';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount = () => {
+    this.courses();
+  }
+  courses = () => {
+    const apiBaseUrl = config.apiBaseUrl;
+    axios.get(apiBaseUrl)
+      .then(response => {
+        console.log(response.data);
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}  
 export default App;
