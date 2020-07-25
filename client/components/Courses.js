d2140904ec9a94a24d1ae267b1b03d1397baa472
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-25 19:24:10 -0400

    add body format

--- client/components/Courses.js
+++ client/components/Courses.js
@@ -1,6 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {NavLink} from 'react-router-dom'
 
 class Courses extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="bounds">
+                //map the array of courses and print each in a div 
+                    <h4 className="course--label"></h4> // labe (courses)
+                    <h3 className="course--title"></h3> // course title
+                </div>
+                <div className="grid-33">
+                    //button styling goes here
+                    <NavLink to="">New Course</NavLink>
+                </div>
+            </Fragment>
+        );
+    }
 
 } 
 export default Courses;
--- client/components/UpdateCourse.js
+++ client/components/UpdateCourse.js
@@ -1,6 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+
 
 class UpdateCourse extends Component {
+
+    render() {
+        return(
+            <Fragment>
+                <h1>Update Course</h1>
+                <div>
+                
+                </div>
+            </Fragment>
+        );
+    }
 
 } 
 export default UpdateCourse;
