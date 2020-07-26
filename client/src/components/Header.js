83b02f24f4050290687623dc153ebe280a2d68f0
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-26 17:42:35 -0400

    add basic header elements

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React,{Fragment} from 'react';
+import {Link} from 'react-router-dom';
 
 function Header() {
+    return(
+        <div class="header">
+            <div className="bounds">
+                <h1 className="header--logo">Courses</h1>
+                <nav>
+                    <a className="signup" href="sign-up.html">Sign Up</a>
+                    <a className="signin" href="sign-in.html">Sign In</a>
+                </nav>
+            </div>
+        </div>
+    
+    ); 
 
 }
 export default Header;
