28111fd35bb458d510c94c0525ee0e3e9c7a9644
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-29 16:39:19 -0400

    add function renders signup validation errors  from the api

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,18 +1,51 @@
 import React from 'react';
 
 export default (props) => {
+    const {
+        cancel,
+        errors,
+        submit,
+        submitButtonText,
+        elements,
+    } = props;
 
-    function handSubmit() {
-
+    function handleSubmit(event) {
+        event.preventDefault();
+        submit();
     }
 
-    function handleCancel() {
-
+    function handleCancel(event) {
+        event.preventDefault();
+        cancel();
     }
 
     return (
-
+        <div>
+            <ErrorsDisplay errors={errors} />
+            <form onSubmit={handleSubmit}>
+                {elements()}
+                <div className="grid-100 pad-bottom">
+                    <button className="button" type="submit">{submitButtonText}</button>
+                    <button className="button button-secondary" onclick={handleCancel}>Cancel</button>
+                </div>
+            </form>
+        </div>
     );
+}}
 
-    function ErrorsDisplay(){}
+function ErrorsDisplay({errors}){
+    let errorsDisplay = null;
+    if(errors.length) {
+        errorsDisplay = (
+            <div>
+                <h2 className="validation--errors--label">validation errors</h2>
+                <div className="validation-errors">
+                    <ur>
+                        {errors.map((error, i) => <li key={i}>{error}</li>)}
+                    </ur>
+                </div>
+            </div>
+        );
+    }
+    return errorsDisplay;
 }
