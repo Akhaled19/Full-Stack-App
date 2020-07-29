63ddb51c542cabdaf9390999ddbdedbfb79a9686
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-29 11:43:03 -0400

    add context.js to manage state

---
+++ client/src/Context.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react';
+import Data from './Data';
+
+const Context = React.createContext();
+
+export default class Provider extends Component {
+    render() {
+        return (
+            <Context.Provider value={}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+
+export const Consumer = Context.Consumer;
+/**
+ * A higher-order component that wraps the provided component in a Context Consumer component.
+ * @param {class} Component - A React component.
+ * @returns {function} A higher-order component.
+ */
+export default function withContext(Component){
+    return function ContextCommponent(props) {
+        return (
+            <Context.Consumer>
+                {context => <Component {...props} context={context} />}
+            </Context.Consumer>
+        );
+    }
+}
