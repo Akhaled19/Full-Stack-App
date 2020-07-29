bf28659105bb15b95ec614ab74ea7087ed5ecefa
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-29 12:01:17 -0400

    add form.js renders validation errors from the API

---
+++ client/src/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default (props) => {
+
+    function handSubmit() {
+
+    }
+
+    function handleCancel() {
+
+    }
+
+    return (
+
+    );
+
+    function ErrorsDisplay(){}
+}
