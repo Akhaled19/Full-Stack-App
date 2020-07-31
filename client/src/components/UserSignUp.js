6c5530303365f876c04de1259151909cbb5e5c4e
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-30 23:54:51 -0400

    add updates to the code

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,7 +1,8 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Form from '../Form';
 
-class UserSignUp extends Component {
+export default class UserSignUp extends Component {
     state = {
         firstName: '',
         lastName: '',
@@ -20,75 +21,65 @@ class UserSignUp extends Component {
             confirmPassword,
             errors, 
         } = this.state;
-        
+
         return(
-            <Fragment>
-                <div class="bounds">
-                    <div className="grid-33 centered signin">
-                        <h1>Sign Up</h1>
-                        <div>
-                            <form>
-                                <div>
-                                    <input 
-                                        id="firstName" 
-                                        name="firstName" 
-                                        type="text"    
-                                        className="" 
-                                        placeholder="First Name" 
-                                        value=""
-                                    required/>
-                                </div>
-                                <div>
-                                    <input 
-                                        id="lastName" 
-                                        name="lastName" 
-                                        type="text" 
-                                        className="" 
-                                        placeholder="Last Name" 
-                                        value="" 
-                                    required/>
-                                </div>
-                                <div>
-                                    <input 
-                                        id="emailAddress" 
-                                        name="emailAddress" 
-                                        type="text" 
-                                        className="" 
-                                        placeholder="Email Address" 
-                                        value=""
-                                    required/>
-                                </div>
-                                <div>
-                                    <input 
-                                        id="password" 
-                                        name="password" 
-                                        type="password" 
-                                        className="" 
-                                        placeholder="Password" 
-                                        value="" 
-                                    required/>
-                                </div>
-                                <div>
-                                    <input 
-                                        id="confirmPassword" 
-                                        name="confirmPassword" 
-                                        type="password" 
-                                        className="" 
-                                        placeholder="Confirm Password"
-                                        value="" 
-                                    required/>
-                                </div>
-                                <div className="grid-100 pad-bottom">
-                                    <button className="button" type="submit">Sign Up</button>
-                                    <button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
-                                </div>
-                            </form>
-                        </div>
-                        <p>&nbsp;</p>
-                        <p>Already have a user account? <Link to="/signin">Click here</Link> to sign in!</p>
-                    </div>
+            <div class="bounds">
+                <div className="grid-33 centered signin">
+                    <h1>Sign Up</h1>
+                    <Form 
+                        cancel={this.cancel}
+                        errors={errors}
+                        submit={this.submit}
+                        submitButtonText="Sign Up"
+                        elments={()=> (
+                            <React.Fragment>
+                                <input 
+                                    id="firstName" 
+                                    name="firstName" 
+                                    type="text"    
+                                    className="" 
+                                    placeholder="First Name" 
+                                    value={firstName}
+                                required/>
+                                <input 
+                                    id="lastName" 
+                                    name="lastName" 
+                                    type="text" 
+                                    className="" 
+                                    placeholder="Last Name" 
+                                    value={lastName}
+                                required/>
+                                <input 
+                                    id="emailAddress" 
+                                    name="emailAddress" 
+                                    type="text" 
+                                    className="" 
+                                    placeholder="Email Address" 
+                                    value={emailAddress}
+                                required/>
+                                <input 
+                                    id="password" 
+                                    name="password" 
+                                    type="password" 
+                                    className="" 
+                                    placeholder="Password" 
+                                    value={password} 
+                                required/>
+                                <input 
+                                    id="confirmPassword" 
+                                    name="confirmPassword" 
+                                    type="password" 
+                                    className="" 
+                                    placeholder="Confirm Password"
+                                    value={confirmPassword} 
+                                required/>
+                            </React.Fragment> 
+                        )}   
+                    />
+                    <p>&nbsp;</p>
+                    <p>Already have a user account? <Link to="/signin">Click here</Link> to sign in!</p>
                 </div>
-          </Fragment>
+            </div>
         )
     }
 
@@ -96,13 +87,21 @@ class UserSignUp extends Component {
     submit = () => {
         const { context } = this.props;
 
+        const {
+            firstName,
+            lastName,
+            emailAddress,
+            password,
+            confirmPassword,
+        } = this.state
+
         //new payload
         const user = {
             firstName,
             lastName,
             emailAddress,
             password,
-            confirmPassword
+            confirmPassword,
         };
         context.data.createUser(user)
             .then(errors => {
@@ -125,4 +124,3 @@ class UserSignUp extends Component {
     };
 
 } 
-export default UserSignUp;
