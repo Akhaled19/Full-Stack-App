28bb5ed1cf1cfe5555944ed3b3ca7e350ae11f0e
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-31 23:07:51 -0400

    subscribe the userSignIn component to context

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,14 @@ import Courses from './components/Courses';
 // import CreatCourse from './components/CreateCourse';
 // import CourseDetail from './components/CourseDetail';
 // import UpdateCourse from './components/UpdateCourse';
-// import UserSignIn from './components/UserSignIn';
+import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
 // import UserSignOut from './components/UserSignOut';
 import NotFound from './components/NotFound';
 
 //Subscribe components to context changes
 const userSignUpWithContext = withContext(UserSignUp);
+const userSignInwithContext = withContext(UserSignIn);
 
 class App extends Component {
   // constructor() {
@@ -41,13 +42,12 @@ class App extends Component {
           <Switch>
             <Route exact path='/' render={ () => <Redirect to='/courses'/>}/>
             <Route path='/courses' component={Courses} />
+            <Route path='/signin' component={userSignInwithContext} />
             <Route path='/signup' component={userSignUpWithContext} />
             <Route component={NotFound}/>
             {/* <Route path='/courses/create' component={CreatCourse} />
             <Route path='/courses/:id/update' component={UpdateCourse} />
             <Route path='courses/:id' component={CourseDetail} />
-            <NavLink path='/signin' component={UserSignIn} />
-            <NavLink path='/signup' component={UserSignUp} />
             <NavLink path='/signout' component={UserSignOut} /> */}
           </Switch>
         </div>
