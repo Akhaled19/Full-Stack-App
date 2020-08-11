b0a1fdb29661b041a3170907c4e3217fe8fc4b13
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-08-11 00:02:26 -0400

    add UpdateCourse component structure

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,60 +1,130 @@
 import React, {Component, Fragment} from 'react';
-import {NavLink} from 'react-router-dom';
-
+import {Link} from 'react-router-dom';
+import Form from '../Form';
 
 class UpdateCourse extends Component {
 
+    state={
+        title: '',
+        description: '',
+        estimatedTime: '',
+        materialsNeeded: '',
+        errors:[] 
+    }
+
     render() {
+        const {
+            title,
+            description,
+            estimatedTime,
+            materialsNeeded,
+            errors 
+        } = this.state;
+
+        const {context} = this.props;
+        const authUser = context.authenticatedUser;
+        Console.log('This the current user', authUser);
         return(
-            <Fragment>
+            <div className="bounds course--detail">
                 <h1>Update Course</h1>
                 <div>
-                    <form>
-                        <div className="grid-66">
-                            <div className="course--header">
-                                <h4 className="course--label">Course</h4>
-                                <div>
-                                    <input 
-                                        id=""
-                                        name=""
-                                        type=""
-                                        className=""
-                                        placeholder="Course title..."
-                                        value=""
-                                    required/>
-                                    <p>Author Name</p>
-                                </div>
-                                <div className="course--description">
-                                    <div>
-                                        <textarea 
-                                            id=""
-                                            name=""
-                                            className=""
-                                            placeholder="Course description..."
-                                            value=""
-                                        required/>
+                    <Form
+                        cancel={this.cancel}
+                        errors={errors}
+                        submit={this.submit}
+                        submitButtonText="Update Course"
+                        elements={()=> (
+                            <Fragment>
+                                <div className="grid-66">
+                                    <div className="course--header">
+                                        <h4 className="course--label">Course</h4>
+                                        <div>
+                                            <input 
+                                                id="title"
+                                                name="title"
+                                                type="text"
+                                                className="input-title course--title--input"
+                                                placeholder="Course title..."
+                                                value={title}
+                                                onChange={this.change}
+                                            />
+                                        </div>
+                                        <p>Author Name</p>
+                                    </div>    
+                                    <div className="course--description">
+                                        <div>
+                                            <textarea 
+                                                id="description"
+                                                name="description"
+                                                placeholder="Course description..."
+                                                value={description}
+                                                onChange={this.change}
+                                            />
+                                        </div>
                                     </div>
+                                    <div className="grid-25 grid-right">
+                                        <div className="course--stats">
+                                            <ul className="course--stats--list">
+                                                <li className="course--stats--list--item">
+                                                    <h4>Estimated Time</h4>
+                                                    <div>
+                                                        <input 
+                                                            id="estimatedTime"
+                                                            name="estimatedTime"
+                                                            type="text"
+                                                            className="course--time--input"
+                                                            placeholder="Hours..."
+                                                            value={estimatedTime}
+                                                            onChange={this.change}
+                                                        />
+                                                    </div>
+                                                </li>
+                                                <li className="course--stats--list--item">
+                                                    <h4>Materials Needed</h4>
+                                                    <div>
+                                                        <textarea 
+                                                            id="materialsNeeded"
+                                                            name="materialsNeeded"
+                                                            placeholder="List materials"
+                                                            value={materialsNeeded}
+                                                            onChange={this.change}
+                                                        />
+                                                    </div>
+                                                </li>
+                                            </ul>   
+                                        </div>
+                                    </div>        
                                 </div>
-                                <div class="grid-25 grid-right">
-                                    <div class="course--stats">
-                                        <ul class="course--stats--list">
-                                        //loop through the course details 
-                                        <li class="course--stats--list--item">
-                                        <h4>Estimated Time</h4>
-                                        </li>
-                                        </ul>   
-                                    </div>
-                                </div>         
-                            </div>
-                        </div>
-                        <div className="grid-100 pad-bottom">
-                            <NavLink></NavLink>
-                        </div>
-                    </form>
+                            </Fragment>
+                        )}
+                    />
                 </div>
-            </Fragment>
+            </div>   
         );
     }
 
+    //update the input fileds 
+    change = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        //update the value
+        this.setState(() => {
+            return{
+                [name]: value 
+            }
+        });
+    }
+
+    //update the course
+    submit = () => {
+
+    }
+
+    //Handle course update cancelation 
+    cancel = () => {
+        //push curent course details route to history stack
+        this.props.history.push(`/courses/${course.id}`); 
+    }
 } 
 export default UpdateCourse;
