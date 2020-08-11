80ff06c38a11de443a4dddece4ef0502d6c66c1e
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-08-11 00:01:12 -0400

    add div wrap

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -27,76 +27,78 @@ class CreateCourse extends Component {
         return(
             <div className="bounds course--detail">
                 <h1>Create Course</h1>
-                <Form 
-                    cancel={this.cancel}
-                    errors={errors}   
-                    submit={this.submit}
-                    submitButtonText="Create Course"
-                    elements={() => (
-                        <Fragment>
-                            <div className="grid-66">
-                                <div className="course--header">
-                                    <h4 className="course--label">Course</h4>
-                                    <div>
-                                        <input 
-                                            id="title" 
-                                            name="title" 
-                                            type="text" 
-                                            className="input-title course--title--input" 
-                                            placeholder="Course title..."
-                                            value={title}
-                                            onChange={this.change}
-                                        />
+                <div>
+                    <Form 
+                        cancel={this.cancel}
+                        errors={errors}   
+                        submit={this.submit}
+                        submitButtonText="Create Course"
+                        elements={() => (
+                            <Fragment>
+                                <div className="grid-66">
+                                    <div className="course--header">
+                                        <h4 className="course--label">Course</h4>
+                                        <div>
+                                            <input 
+                                                id="title" 
+                                                name="title" 
+                                                type="text" 
+                                                className="input-title course--title--input" 
+                                                placeholder="Course title..."
+                                                value={title}
+                                                onChange={this.change}
+                                            />
+                                        </div>
+                                        <p>By {authUser.firstName} {authUser.lastName}</p>
                                     </div>
-                                    <p>By {authUser.firstName} {authUser.lastName}</p>
-                                </div>
-                                <div className="course--description">
-                                    <div>
-                                        <textarea 
-                                            id="description" 
-                                            name="description" 
-                                            placeholder="Course description..."
-                                            value={description}
-                                            onChange={this.change}
-                                        />
+                                    <div className="course--description">
+                                        <div>
+                                            <textarea 
+                                                id="description" 
+                                                name="description" 
+                                                placeholder="Course description..."
+                                                value={description}
+                                                onChange={this.change}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>    
+                                <div className="grid-25 grid-right">
+                                    <div className="course--stats">
+                                        <ul className="course--stats--list">
+                                            <li className="course--stats--list--item">
+                                                <h4>Estimated Time</h4>
+                                                <div>
+                                                    <input 
+                                                        id="estimatedTime" 
+                                                        name="estimatedTime" 
+                                                        type="text" 
+                                                        className="course--time--input"
+                                                        placeholder="Hours" 
+                                                        value={estimatedTime}
+                                                        onChange={this.change}
+                                                    />
+                                                </div>
+                                            </li>
+                                            <li className="course--stats--list--item">
+                                                <h4>Materials Needed</h4>
+                                                <div>
+                                                    <textarea 
+                                                        id="materialsNeeded" 
+                                                        name="materialsNeeded" 
+                                                        placeholder="List materials..."
+                                                        value={materialsNeeded}
+                                                        onChange={this.change}
+                                                    />
+                                                </div>
+                                            </li>
+                                        </ul>
                                     </div>
                                 </div>
-                            </div>    
-                            <div className="grid-25 grid-right">
-                                <div className="course--stats">
-                                    <ul className="course--stats--list">
-                                        <li className="course--stats--list--item">
-                                            <h4>Estimated Time</h4>
-                                            <div>
-                                                <input 
-                                                    id="estimatedTime" 
-                                                    name="estimatedTime" 
-                                                    type="text" 
-                                                    className="course--time--input"
-                                                    placeholder="Hours" 
-                                                    value={estimatedTime}
-                                                    onChange={this.change}
-                                                />
-                                            </div>
-                                        </li>
-                                        <li className="course--stats--list--item">
-                                            <h4>Materials Needed</h4>
-                                            <div>
-                                                <textarea 
-                                                    id="materialsNeeded" 
-                                                    name="materialsNeeded" 
-                                                    placeholder="List materials..."
-                                                    value={materialsNeeded}
-                                                    onChange={this.change}
-                                                />
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </Fragment>
-                    )}
-                />
+                            </Fragment>
+                        )}
+                    />
+                </div>>
             </div>    
         )
     }
